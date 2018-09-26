5ce63da36cc0770691b6634505cdae0e6dc63519
naren <narenravi92@gmail.com>
2018-09-26 06:44:56 +0530

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: 'dudewho.codes',
+    description: 'Portfolio of Naren. I am a Software Consultant and I have worked with \
+    variety of technologies. I am good at backend stuffs.',
+    keywords: 'naren, software, consultant, software consultant, backend, python, golang, aws, azure, \
+    architect, cloud'
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
