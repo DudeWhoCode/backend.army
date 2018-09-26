276d190ac125cffbef4bc9fa22381c11595d7026
naren <narenravi92@gmail.com>
2018-09-26 07:01:25 +0530

    Customize portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,18 +31,18 @@ const IndexPage = () => (
   <div>
     <div className="Hero" >
       <div className="HeroGroup">
-        <h1>Hi people</h1>
+        <h1>NARENDRAN</h1>
         <br />
-        <p>Welcome to my portfolio</p>  
-        <p>I am a software consultant.</p>
+        <p>Software Consultant</p>  
+        <p>backend | cloud architecting | product engineering</p>
         <Link to="/page-2/">Action item</Link>
         <div className="Logos">
-          <img src={require('../images/logo-sketch.png')} width="50"></img>
-          <img src={require('../images/logo-figma.png')} width="50"></img>
-          <img src={require('../images/logo-studio.png')} width="50"></img>
-          <img src={require('../images/logo-framer.png')} width="50"></img>
+          <img src={require('../images/logo-python.png')} width="50"></img>
+          <img src={require('../images/logo-go.png')} width="50"></img>
+          <img src={require('../images/logo-aws.png')} width="50"></img>
+          <img src={require('../images/logo-azure.png')} width="50"></img>
+          <img src={require('../images/logo-postgres.png')} width="50"></img>
           <img src={require('../images/logo-react.png')} width="50"></img>
-          <img src={require('../images/logo-swift.png')} width="50"></img>
         </div>
         <Wave />
       </div>
